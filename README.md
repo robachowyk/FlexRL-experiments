c8c15c9eba12061daafc4c1fe019c05b95fcd2a0
Kayané Robach <kaya.robach@gmail.com>
2024-06-05 15:20:16 +0200

    Update repo
    Clean code and drafts.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FlexRL
- Stochastic EM Flexible Record Linkage
+
+This package implements the Stochastic EM approach to Record Linkage described in ... and developed by _Kayané Robach_ and _Michel Hof_.
+
+For support requests, contact _k dot c dot robach at amsterdamumc dot nl_.
--- functions.cpp
+++ functions.cpp
@@ -9,16 +9,32 @@
 // [[Rcpp::depends(RcppArmadillo)]] 
 using namespace Rcpp;
 
-static std::random_device rd; 
-
-// initialize Mersennes' twister using rd to generate the seed
-static std::mt19937 gen{rd()}; 
-std::uniform_real_distribution<double> dist(0, 1);
+// static std::random_device rd; 
+// 
+// // initialize Mersennes' twister using rd to generate the seed
+// static std::mt19937 gen{rd()}; 
+// std::uniform_real_distribution<double> dist(0, 1);
 
 // [[Rcpp::export]]
 NumericVector callFunction(NumericVector x, Function f) {
   NumericVector res = f(x);
   return res;
+}
+
+// [[Rcpp::export]]
+double ControlRandomNumberGen() {
+  // Random number smaller than prob -> generate binomial value
+  double randomnumber = R::runif(0,1);
+  // std::cout << "randomnumber" << randomnumber << std::endl;
+  return randomnumber;
+}
+
+// [[Rcpp::export]]
+double ControlRandomSampleGen(IntegerVector choiceset, NumericVector probavec) {
+  // std::cout << "probavec" << probavec << std::endl;
+  double generatedrandom = Rcpp::sample(choiceset, 1, false, probavec)[0];
+  // std::cout << "generatedrandom" << generatedrandom << std::endl;
+  return generatedrandom;
 }
 
 // [[Rcpp::export]] 
@@ -147,6 +163,38 @@ List F1(const IntegerVector & HA, const IntegerVector & HB, const int & nvals)
   return out;
 }
 
+// NumericMatrix getUniqueRows(IntegerMatrix mat) {
+//   // Convert matrix to DataFrame to use unique() function
+//   DataFrame df = as<DataFrame>(mat);
+//   // Get unique rows
+//   DataFrame unique_df = unique(df);
+//   // Convert back to matrix
+//   NumericMatrix unique_mat(unique_df);
+//   return unique_mat;
+// }
+// 
+// // [[Rcpp::export]]
+// List getUniqueSetsOfRows(IntegerMatrix A, IntegerMatrix B) {
+//   // Get unique rows for each matrix
+//   
+//   unique(v)
+//   
+//   NumericMatrix unique_rows_mat1 = getUniqueRows(A);
+//   NumericMatrix unique_rows_mat2 = getUniqueRows(B);
+//   // Return list of unique rows for each matrix
+//   List ret;
+//   ret["unique_rows_mat1"] = unique_rows_mat1;
+//   ret["unique_rows_mat2"] = unique_rows_mat2;
+//   return ret;
+// }
+// 
+// // [[Rcpp::export]] 
+// List NaiveBlocking(IntegerMatrix A, IntegerMatrix B)
+// {
+//   List ret = getUniqueSetsOfRows(A, B);
+//   return ret;
+// }
+
 // [[Rcpp::export]] 
 List sampleD(const IntegerMatrix & S,
              const NumericVector & LLA, 
@@ -176,7 +224,7 @@ List sampleD(const IntegerMatrix & S,
       double sumlogdensity = log(1 + exp(loglik-loglikNew)) + loglikNew;
       double pswitch = exp(loglikNew - sumlogdensity);		
       // Random number smaller than prob -> generate binomial value
-      bool link = dist(gen) < pswitch;
+      bool link = ControlRandomNumberGen()  < pswitch;
       if(link)
       {
         loglik = loglikNew;
@@ -196,8 +244,8 @@ List sampleD(const IntegerMatrix & S,
       + log(1-gamma(i)) - log(gamma(i))
       + log(LLB.length() - nlinkrec+1);
       double sumlogdensity = log(1 + exp(loglik-loglikNew)) + loglikNew;	
-      double pswitch = exp(loglikNew - sumlogdensity);		
-      bool nolink = dist(gen) < pswitch;
+      double pswitch = exp(loglikNew - sumlogdensity);	
+      bool nolink = ControlRandomNumberGen()  < pswitch;
       if(nolink)
       {
         loglik = loglikNew;
@@ -247,14 +295,14 @@ IntegerVector sampleNL(IntegerVector G, NumericVector eta, NumericVector phi)
     if(G(i) == 0)
     { 
       // All equally likely
-      H(i) = Rcpp::sample(choice_set, 1, false, eta)[0];
+      H(i) = ControlRandomSampleGen(choice_set, eta);
     }else
     {
       NumericVector help1(nval, pTypo);   
       help1(G(i)-1) = pAgree; 
       // Joint probability to have the registered and true value
-      NumericVector prob = eta * help1;  
-      H(i) = Rcpp::sample(choice_set, 1, false, prob)[0];
+      NumericVector prob = eta * help1; 
+      H(i) = ControlRandomSampleGen(choice_set, prob);
     }
   }
   return H;
@@ -304,24 +352,24 @@ IntegerVector sampleL(IntegerVector GA, IntegerVector GB, NumericVector survival
     if(GA(i)==0 && GB(i)==0)
     {
       NumericVector prob = eta * probH;
-      H(i) = Rcpp::sample(choice_index, 1, false, prob)[0];
+      H(i) = ControlRandomSampleGen(choice_index, prob);
     }else if(GA(i)>0 && GB(i)==0)
     {
       // Joint probability to have the registered and true value
       NumericVector prob = eta * probH * helpA;  
-      H(i) = Rcpp::sample(choice_index, 1, false, prob)[0];
+      H(i) = ControlRandomSampleGen(choice_index, prob);
     }else if(GA(i)==0 && GB(i)>0)
     {
       // Joint probability to have the registered and true value
       NumericVector prob = eta * probH  * helpB;  
-      H(i) = Rcpp::sample(choice_index, 1, false, prob)[0];
+      H(i) = ControlRandomSampleGen(choice_index, prob);
     }else if(GA(i)>0 && GB(i)>0)
     {  
       // None missing
       // Create vectors indicating P(Registered=X|True)
       // Joint probability to have the registered and true value
       NumericVector prob = eta * probH * helpA * helpB; 
-      H(i) = Rcpp::sample(choice_index, 1, false, prob)[0];
+      H(i) = ControlRandomSampleGen(choice_index, prob);
     }   
   }
   return H;
@@ -356,7 +404,7 @@ IntegerVector generateSequence(int n) {
 }
 
 // [[Rcpp::export]]
-List sampleH(IntegerVector nA, IntegerVector nB, IntegerMatrix links, List omegaData, LogicalVector pivs_stable, List pivsA, List pivsB, IntegerVector nvalues, arma::sp_mat D, LogicalVector nonlinkedA, LogicalVector nonlinkedB, List eta, List omega, List phi)
+List sampleH(IntegerVector nA, IntegerVector nB, IntegerMatrix links, List omegaData, LogicalVector pivs_stable, List pivsA, List pivsB, IntegerVector nvalues, arma::sp_mat D, LogicalVector nonlinkedA, LogicalVector nonlinkedB, List eta, List phi)
 {
   IntegerMatrix truepivsA(nA[0],nA[1]);
   IntegerMatrix truepivsB(nB[0],nB[1]);
@@ -412,102 +460,102 @@ List sampleH(IntegerVector nA, IntegerVector nB, IntegerMatrix links, List omega
   return ret;
 }
 
-// [[Rcpp::export]]
-List sampleH_old(IntegerVector nA, IntegerVector nB, NumericVector times, List Xomegaomega, LogicalVector pivs_stable, List pivsA, List pivsB, IntegerMatrix links, IntegerVector nvalues, arma::sp_mat D, LogicalVector nonlinkedA, LogicalVector nonlinkedB, List eta, List omega, List phi)
-{
-  IntegerMatrix truepivsA(nA[0],nA[1]);
-  IntegerMatrix truepivsB(nB[0],nB[1]);
-  
-  int nphi = 2;
-  for (int k = 0; k < nvalues.length(); k++) {
-    NumericVector eta_k = eta(k);
-    NumericVector phi_k = phi(k);
-    
-    NumericVector phi_k_A(nphi);
-    phi_k_A(0) = phi_k(0);
-    phi_k_A(1) = phi_k(1);
-    NumericVector phi_k_B(nphi);
-    phi_k_B(0) = phi_k(0);
-    phi_k_B(1) = phi_k(2);
-    
-    IntegerVector pivsA_k = pivsA(k);
-    IntegerVector pivsB_k = pivsB(k);
-    IntegerVector idxlinksA = links(_,0);
-    IntegerVector idxlinksB = links(_,1);
-    
-    IntegerVector pivsA_k_L(links.nrow());
-    IntegerVector pivsB_k_L(links.nrow());
-    for (int i=0; i < links.nrow(); i++){
-      int idxA_L = idxlinksA(i) - 1;
-      pivsA_k_L(i) = pivsA_k(idxA_L);
-      int idxB_L = idxlinksB(i) - 1;
-      pivsB_k_L(i) = pivsB_k(idxB_L);
-    }
-    
-    IntegerVector pivsA_k_NL = pivsA_k[nonlinkedA];
-    IntegerVector pivsB_k_NL = pivsB_k[nonlinkedB];
-    
-    IntegerVector truepivsA_k_NL = sampleNL(pivsA_k_NL, eta_k, phi_k_A);
-    IntegerVector truepivsB_k_NL = sampleNL(pivsB_k_NL, eta_k, phi_k_B);
-    
-    IntegerMatrix choice_set;
-    IntegerVector choice_equal;
-    NumericVector eta_choice;
-    
-    if (links.nrow()>0)
-    {
-      IntegerVector values1 = generateSequence(nvalues(k));
-      IntegerVector values2 = generateSequence(nvalues(k));
-      choice_set = ExpandGrid(values1, values2);
-      choice_equal = choice_set(_,1) == choice_set(_,0);
-      IntegerVector choice_set_idx_eta(choice_set.nrow());
-      
-      for (int i=0; i < choice_set.nrow(); i++){
-        IntegerVector choice_vect = choice_set(_,1);
-        choice_set_idx_eta(i) = choice_vect(i) - 1;
-      }
-      
-      eta_choice = eta_k[choice_set_idx_eta];
-      NumericVector survivalpSameH(times.length(), 1);
-      if(!pivs_stable[k])
-      {
-        NumericVector Xomegaomega_k = Xomegaomega[k];
-        survivalpSameH = exp(-Xomegaomega_k);
-      }
-      
-      IntegerVector out = sampleL(pivsA_k_L, pivsB_k_L, survivalpSameH, choice_set, choice_equal, nvalues(k), phi_k_A, phi_k_B, eta_choice);
-      IntegerVector choice_set_A = choice_set(_,0);
-      // if need to print:
-      // std::cout << "choice_set_A" << choice_set_A << std::endl;
-      IntegerVector choice_set_B = choice_set(_,1);
-      for (int i=0; i < links.nrow(); i++){
-        int idxA_L = idxlinksA(i) - 1;
-        int idxA_choice = out(i) - 1;
-        truepivsA(idxA_L,k) = choice_set_A(idxA_choice);
-        int idxB_L = idxlinksB(i) - 1;
-        int idxB_choice = out(i) - 1;
-        truepivsB(idxB_L,k) = choice_set_B(idxB_choice);
-      }
-    }
-    int m = 0;
-    for (int i=0; i < nonlinkedA.length(); i++) {
-      if (nonlinkedA(i)){
-        truepivsA(i,k) = truepivsA_k_NL(m);
-        // truepivsA_k[i] = truepivsA_k_NL[m];
-        m++;
-      }
-    }
-    int n = 0;
-    for (int i=0; i < nonlinkedB.length(); i++) {
-      if (nonlinkedB(i)){
-        truepivsB(i,k) = truepivsB_k_NL(n);
-        // truepivsB_k[i] = truepivsB_k_NL[n];
-        n++;
-      }
-    }
-  }
-  List ret;
-  ret["truepivsA"] = truepivsA;
-  ret["truepivsB"] = truepivsB;
-  return ret;
-}
+// // [[Rcpp::export]]
+// List sampleH_old(IntegerVector nA, IntegerVector nB, NumericVector times, List Xomegaomega, LogicalVector pivs_stable, List pivsA, List pivsB, IntegerMatrix links, IntegerVector nvalues, arma::sp_mat D, LogicalVector nonlinkedA, LogicalVector nonlinkedB, List eta, List omega, List phi)
+// {
+//   IntegerMatrix truepivsA(nA[0],nA[1]);
+//   IntegerMatrix truepivsB(nB[0],nB[1]);
+//   
+//   int nphi = 2;
+//   for (int k = 0; k < nvalues.length(); k++) {
+//     NumericVector eta_k = eta(k);
+//     NumericVector phi_k = phi(k);
+//     
+//     NumericVector phi_k_A(nphi);
+//     phi_k_A(0) = phi_k(0);
+//     phi_k_A(1) = phi_k(1);
+//     NumericVector phi_k_B(nphi);
+//     phi_k_B(0) = phi_k(0);
+//     phi_k_B(1) = phi_k(2);
+//     
+//     IntegerVector pivsA_k = pivsA(k);
+//     IntegerVector pivsB_k = pivsB(k);
+//     IntegerVector idxlinksA = links(_,0);
+//     IntegerVector idxlinksB = links(_,1);
+//     
+//     IntegerVector pivsA_k_L(links.nrow());
+//     IntegerVector pivsB_k_L(links.nrow());
+//     for (int i=0; i < links.nrow(); i++){
+//       int idxA_L = idxlinksA(i) - 1;
+//       pivsA_k_L(i) = pivsA_k(idxA_L);
+//       int idxB_L = idxlinksB(i) - 1;
+//       pivsB_k_L(i) = pivsB_k(idxB_L);
+//     }
+//     
+//     IntegerVector pivsA_k_NL = pivsA_k[nonlinkedA];
+//     IntegerVector pivsB_k_NL = pivsB_k[nonlinkedB];
+//     
+//     IntegerVector truepivsA_k_NL = sampleNL(pivsA_k_NL, eta_k, phi_k_A);
+//     IntegerVector truepivsB_k_NL = sampleNL(pivsB_k_NL, eta_k, phi_k_B);
+//     
+//     IntegerMatrix choice_set;
+//     IntegerVector choice_equal;
+//     NumericVector eta_choice;
+//     
+//     if (links.nrow()>0)
+//     {
+//       IntegerVector values1 = generateSequence(nvalues(k));
+//       IntegerVector values2 = generateSequence(nvalues(k));
+//       choice_set = ExpandGrid(values1, values2);
+//       choice_equal = choice_set(_,1) == choice_set(_,0);
+//       IntegerVector choice_set_idx_eta(choice_set.nrow());
+//       
+//       for (int i=0; i < choice_set.nrow(); i++){
+//         IntegerVector choice_vect = choice_set(_,1);
+//         choice_set_idx_eta(i) = choice_vect(i) - 1;
+//       }
+//       
+//       eta_choice = eta_k[choice_set_idx_eta];
+//       NumericVector survivalpSameH(times.length(), 1);
+//       if(!pivs_stable[k])
+//       {
+//         NumericVector Xomegaomega_k = Xomegaomega[k];
+//         survivalpSameH = exp(-Xomegaomega_k);
+//       }
+//       
+//       IntegerVector out = sampleL(pivsA_k_L, pivsB_k_L, survivalpSameH, choice_set, choice_equal, nvalues(k), phi_k_A, phi_k_B, eta_choice);
+//       IntegerVector choice_set_A = choice_set(_,0);
+//       // if need to print:
+//       // std::cout << "choice_set_A" << choice_set_A << std::endl;
+//       IntegerVector choice_set_B = choice_set(_,1);
+//       for (int i=0; i < links.nrow(); i++){
+//         int idxA_L = idxlinksA(i) - 1;
+//         int idxA_choice = out(i) - 1;
+//         truepivsA(idxA_L,k) = choice_set_A(idxA_choice);
+//         int idxB_L = idxlinksB(i) - 1;
+//         int idxB_choice = out(i) - 1;
+//         truepivsB(idxB_L,k) = choice_set_B(idxB_choice);
+//       }
+//     }
+//     int m = 0;
+//     for (int i=0; i < nonlinkedA.length(); i++) {
+//       if (nonlinkedA(i)){
+//         truepivsA(i,k) = truepivsA_k_NL(m);
+//         // truepivsA_k[i] = truepivsA_k_NL[m];
+//         m++;
+//       }
+//     }
+//     int n = 0;
+//     for (int i=0; i < nonlinkedB.length(); i++) {
+//       if (nonlinkedB(i)){
+//         truepivsB(i,k) = truepivsB_k_NL(n);
+//         // truepivsB_k[i] = truepivsB_k_NL[n];
+//         n++;
+//       }
+//     }
+//   }
+//   List ret;
+//   ret["truepivsA"] = truepivsA;
+//   ret["truepivsB"] = truepivsB;
+//   return ret;
+// }
